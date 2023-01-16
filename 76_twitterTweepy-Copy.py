855c4b5a2c9d0aa00308102ba3784b6b5e1604c0
Semih Durmaz <51100038+SemihDurmaz54@users.noreply.github.com>
2023-01-16 15:19:34 +0100

    Algorith to scrap tweets

---
+++ 76_twitterTweepy-Copy.py
@@ -0,0 +1,38 @@
+import tweepy  # https://github.com/tweepy/tweepy
+import csv
+import pandas as pd
+
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+
+count = 0
+
+def get_all_tweets(screen_name):
+    global count
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    alltweets = []
+    new_tweets = api.user_timeline(screen_name=screen_name, count=1000)
+    alltweets.extend(new_tweets)
+    outtweets = [[tweet.id, tweet.text] for tweet in alltweets]
+
+    with open(f'C:/Users/Semih/PycharmProjects/firstProjectEver/eclat/Apriori-Eclat-master/mixed/long/{count}_{screen_name}_long-tweets.csv', 'w',encoding="utf-8",errors='ignore') as f:
+        writer = csv.writer(f)
+        writer.writerow(["name","text"])
+        writer.writerows(outtweets)
+
+    pass
+
+
+if __name__ == '__main__':
+
+    df = pd.read_csv("users.csv",sep=",", encoding="ISO-8859-1")
+    for x in range(len(df)):
+        count+=1
+        name = str(df.values[x][0])
+        get_all_tweets(name)
